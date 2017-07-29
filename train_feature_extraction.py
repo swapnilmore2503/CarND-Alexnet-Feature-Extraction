89edbba879f6c91b09c5db228c9c5a3a0659b635
swapnilmore2503 <smore@clemson.edu>
2017-07-29 19:46:37 -0400

    First commit

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -2,24 +2,85 @@ import pickle
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from alexnet import AlexNet
-
+from sklearn.utils import shuffle
 # TODO: Load traffic signs data.
+training_file = 'train.p'
+with open(training_file, mode = 'rb') as f:
+    train = pickle.load(f)
 
+X, Y = train['features'], train['labels']
 # TODO: Split data into training and validation sets.
+X_train, X_valid, y_train, y_valid = train_test_split(X, Y, test_size = 0.2, random_state = 42)
 
 # TODO: Define placeholders and resize operation.
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+resized_x = tf.image.resize_images(x, (227, 227))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, 43)
+
+assert resized_x is not Ellipsis
 
 # TODO: pass placeholder as first argument to `AlexNet`.
-fc7 = AlexNet(..., feature_extract=True)
+fc7 = AlexNet(resized_x, feature_extract=True)
 # NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
 # past this point, keeping the weights before and up to `fc7` frozen.
 # This also makes training faster, less work to do!
 fc7 = tf.stop_gradient(fc7)
 
 # TODO: Add the final layer for traffic sign classification.
+fc8_W = tf.Variable(tf.truncated_normal([4096, 43], mean = 0.0, stddev = 0.1))
+fc8_b = tf.Variable(tf.truncated_normal([43], mean = 0.0, stddev = 0.1))
 
+logits = tf.add(tf.matmul(fc7, fc8_W), fc8_b)
+
+# Define hyperparameters
+epochs = 15
+l_rate = 0.0005
+batch_size = 128
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels = one_hot_y, logits = logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = l_rate)
+training_operation = optimizer.minimize(loss_operation) 
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
 
 # TODO: Train and evaluate the feature extraction model.
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, batch_size):
+        batch_x, batch_y = X_data[offset:offset+batch_size], y_data[offset:offset+batch_size]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+# Training the model
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+    
+    print("Training...")
+    print()
+    # Train the model
+    for i in range(epochs):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, batch_size):
+            end = offset + batch_size
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            tr, loss = sess.run([training_operation, loss_operation], feed_dict={x: batch_x, y: batch_y})
+            
+        # Validate model accuracy   
+        validation_accuracy = evaluate(X_valid, y_valid)
+        print("EPOCH {} ...".format(i+1))
+        print("Loss = {:.6f}".format(loss))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+        
+    saver.save(sess, './AlexTraffic.ckpt')
+    print("Model saved")
